5901fb04be4af9ff082340718d00975d4e77dea3
neil2025 <neilkush30@gmail.com>
2022-05-06 07:32:13 -0400

    Class Changing V2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // send messages
-let form = document.getElementById('sendForm')
+var form = document.getElementById('sendForm')
 var messagePath = '';
+var courses;
 let send = (e) => {
     e.preventDefault();
     let Content = form.elements[0].value;
     let User = form.elements[1].value;
-    const db = ref(database, path = `messages/${messagePath}${Date.now()}`);
+    window.localStorage.setItem('username', User)
+    const db = ref(database, path = `messages/${messagePath}/${Date.now()}`);
     set(db, {
         msg: Content,
         user: User
@@ -16,9 +18,15 @@ let send = (e) => {
 form.addEventListener('submit', send);
 // append new changes to 'output' div
 
-onValue(ref(database, 'messages/'), (snap) => {
-    let outputString = "";
-    for (key in snap.val()) {
+onValue(ref(database, `messages/`), (snap) => {
+    let value;
+    if (messagePath !== '') {
+        value = snap.val()[messagePath]
+    } else if (messagePath == '') {
+        value = snap.val()
+    }
+    var outputString = "";
+    for (key in value) {
         outputString += `${snap.val()[key].user}: ${snap.val()[key].msg}` + "\n";
     };
     document.getElementById('textOutput').innerHTML = outputString;
@@ -27,46 +35,99 @@ onValue(ref(database, 'messages/'), (snap) => {
 // get canvas data
 // API KEY: 4851~XAVozN0ZoKzbGzkS5nabWUpQ0yHtwx1FCpMt40EACu55NASF2pFg13BNm48kF8YG
 
+try {
+    courses = JSON.parse(window.localStorage.getItem('studentCourses'));
+    form.elements[1].value = window.localStorage.getItem('username');
+    makeButtons(courses);
+} catch (err) {
+    document.getElementById('login').style.display = 'inline-block';
+}
+
 function getCourses() {
     const URL = "https://dwight.instructure.com/api/v1/courses";
-    // const URL = "https://jsonplaceholder.typicode.com/todos/2";
-    // let params = {
-    //     headers: {
-    //         'Authorization': 'Bearer 4851~XAVozN0ZoKzbGzkS5nabWUpQ0yHtwx1FCpMt40EACu55NASF2pFg13BNm48kF8YG',
-    //     },
-    //     method: "GET",
-    //     mode: 'no-cors'
-    // };
-    // fetch(URL, params)
-    // .then(raw => raw.json())
-    // .then(data => {console.log(data)})
     let newWin = window.open(URL, '', 'popup');
+}
+
+function makeButtons(cs) {
+    let classes = document.getElementById('courses');
+    for (let item of cs) {
+        let inp = document.createElement('input');
+        inp.setAttribute('id', item.ClassID);
+        inp.setAttribute('class', 'courseButton');
+        inp.setAttribute('type', 'button');
+        inp.setAttribute('value', item.Name);
+
+        let newDiv = document.createElement('div');
+        newDiv.appendChild(inp);
+        classes.appendChild(newDiv);
+
+        document.getElementById(item.ClassID).addEventListener('click', e => {
+            messagePath = `messages/${e.target.id}`;
+            let reference = ref(database, messagePath);
+
+            try {
+                get(child(reference, messagePath)).then((snap) => {
+                    outputString = '';
+                    if (snap.exists()) {
+                        for (key in snap.val()) {
+                            outputString += `${snap.val()[key].user}: ${snap.val()[key].msg}` + "\n";
+                        };
+                    }
+
+                })
+            } catch (err) {
+
+            })
+
+    };
+
+    get(child(reference, messagePath)).then((snap) => {
+        outputString = '';
+        if (snap.exists()) {
+            for (key in snap.val()) {
+                outputString += `${snap.val()[key].user}: ${snap.val()[key].msg}` + "\n";
+            };
+        } else {
+            let time = Date.now();
+            set(reference,
+                {
+                    [time]: {
+                        msg: `Welcome  to  ${e.target.value}!`,
+                        user: form.elements[1].value,
+                    }
+                })
+            for (key in snap.val()) {
+                outputString += `${snap.val()[key].user}: ${snap.val()[key].msg}` + "\n";
+            };
+
+        })
+
+
+    get(child(reference, messagePath)).then((snap) => {
+        outputString = '';
+        if (snap.exists()) {
+            for (key in snap.val()) {
+                outputString += `${snap.val()[key].user}: ${snap.val()[key].msg}` + "\n";
+            };
+        }
+
+    })
+
+
+})
+    }
 }
 
 document.getElementById('getCourses').addEventListener('submit', (e) => {
     e.preventDefault();
     let entry = document.getElementById('coursesEntry');
-    var courses = JSON.parse(entry.value).filter(o => (o.start_at.split('-')[0] == '2021')).map((i) => {
+    courses = JSON.parse(entry.value).filter(o => (o.start_at.split('-')[0] == '2021')).map((i) => {
         return {
             Name: i.name,
             ClassID: i.id,
         }
     })
-    console.log(courses);
+    window.localStorage.setItem('studentCourses', JSON.stringify(courses));
     entry.value = '';
-    let classes = document.getElementById('courses');
-    for (let item of courses) {
-        let inp = document.createElement('input');
-        inp.setAttribute('id', item.Name);
-        inp.setAttribute('class', 'courseButton');
-        inp.setAttribute('type', 'radio');
-        let lab = document.createElement('label');
-        lab.setAttribute('for', item.Name);
-        lab.appendChild(document.createTextNode(item.Name));
-        let newDiv = document.createElement('div');
-        newDiv.appendChild(inp);
-        newDiv.appendChild(lab);
-        classes.appendChild(newDiv);
-
-    }
+    makeButtons(courses);
 })
